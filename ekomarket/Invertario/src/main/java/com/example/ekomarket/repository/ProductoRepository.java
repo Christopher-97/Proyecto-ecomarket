66a540857ed8cb74f9bbc83fd2215a09c4916102
Christopher <135161034+Christopher-97@users.noreply.github.com>
2025-07-11 15:44:35 -0400

    Delete ekomarket/Invertario directory

--- ekomarket/Invertario/src/main/java/com/example/ekomarket/repository/ProductoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.ekomarket.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.ekomarket.model.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {}
